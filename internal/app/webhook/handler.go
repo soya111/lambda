b5399f7bb8fab3879fda3267de74201fbfdde8b3
soya111 <soyak20000316@gmail.com>
2022-02-09 00:08:11 +0900

    add member list

--- internal/app/webhook/handler.go
+++ internal/app/webhook/handler.go
@@ -19,33 +19,6 @@ import (
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
-var (
-	memberList = []string{
-		"潮紗理菜",
-		"影山優佳",
-		"加藤史帆",
-		"齊藤京子",
-		"佐々木久美",
-		"佐々木美玲",
-		"高瀬愛奈",
-		"高本彩花",
-		"東村芽依",
-		"金村美玖",
-		"河田陽菜",
-		"小坂菜緒",
-		"富田鈴花",
-		"丹生明里",
-		"濱岸ひより",
-		"松田好花",
-		"宮田愛萌",
-		"渡邉美穂",
-		"上村ひなの",
-		"髙橋未来虹",
-		"森本茉莉",
-		"山口陽世",
-	}
-)
-
 func HandleTextMessage(t string, event *linebot.Event) {
 	text := strings.Split(t, " ")
 	switch {
@@ -187,7 +160,7 @@ func showSubscribeList(event *linebot.Event) {
 		return
 	}
 
-	message := "登録リスト\n"
+	message := "登録リスト"
 	for _, v := range list {
 		message += fmt.Sprintf("\n%s", v.MemberName)
 	}

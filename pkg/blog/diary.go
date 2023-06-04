c2b5446bad45a8de130d44124e2c6de44030404e
soya111 <soyak20000316@gmail.com>
2023-06-04 17:00:40 +0900

    install guregu to diary repository

--- pkg/blog/diary.go
+++ pkg/blog/diary.go
@@ -1,13 +1,10 @@
 package blog
 
 import (
-	"strconv"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/guregu/dynamo"
 )
 
 var (
@@ -16,12 +13,12 @@ var (
 )
 
 type Diary struct {
-	Url        string `dynamodbav:"url" json:"url"`
-	Title      string `dynamodbav:"title" json:"title"`
-	MemberName string `dynamodbav:"member_name" json:"member_name"`
-	Date       string `dynamodbav:"date" json:"date"`
-	Id         int    `dynamodbav:"diary_id" json:"diary_id"`
-	Images     []string
+	Url        string   `dynamo:"url" json:"url"`
+	Title      string   `dynamo:"title" json:"title"`
+	MemberName string   `dynamo:"member_name" json:"member_name"`
+	Date       string   `dynamo:"date" json:"date"`
+	Id         int      `dynamo:"diary_id" json:"diary_id"`
+	Images     []string `dynamo:"images,omitempty" json:"images"`
 }
 
 func NewDiary(url string, title string, memberName string, date time.Time, id int) *Diary {
@@ -43,63 +40,30 @@ type DiaryRepository interface {
 }
 
 type DynamoDiaryRepository struct {
-	db        *dynamodb.DynamoDB
-	tableName string
+	db    *dynamo.DB
+	table dynamo.Table
 }
 
 func NewDynamoDiaryRepository(sess *session.Session, tableName string) *DynamoDiaryRepository {
+	db := dynamo.New(sess)
+	table := db.Table(tableName)
 	return &DynamoDiaryRepository{
-		db:        dynamodb.New(sess),
-		tableName: tableName,
+		db:    db,
+		table: table,
 	}
 }
 
-// DynamoからGET
-func (repo *DynamoDiaryRepository) GetDiary(memberName string, diaryId int) (*Diary, error) {
-	getParam := &dynamodb.GetItemInput{
-		TableName: aws.String(repo.tableName),
-		Key: map[string]*dynamodb.AttributeValue{
-			"member_name": {
-				S: aws.String(memberName),
-			},
-			"diary_id": {
-				N: aws.String(strconv.Itoa(diaryId)),
-			},
-		},
-	}
-
-	result, err := repo.db.GetItem(getParam)
+func (r *DynamoDiaryRepository) GetDiary(memberName string, diaryId int) (*Diary, error) {
+	diary := new(Diary)
+	err := r.table.Get("member_name", memberName).Range("diary_id", dynamo.Equal, diaryId).One(diary)
 	if err != nil {
 		return nil, err
 	}
-
-	diary := Diary{}
-	err = dynamodbattribute.UnmarshalMap(result.Item, &diary)
-	if err != nil {
-		return nil, err
-	}
-
-	return &diary, nil
+	return diary, nil
 }
 
-// DynamoにPOST
-func (repo *DynamoDiaryRepository) PostDiary(diary *Diary) error {
-	inputAV, err := dynamodbattribute.MarshalMap(diary)
-	if err != nil {
-		return err
-	}
-
-	input := &dynamodb.PutItemInput{
-		TableName: aws.String(repo.tableName),
-		Item:      inputAV,
-	}
-
-	_, err = repo.db.PutItem(input)
-	if err != nil {
-		return err
-	}
-
-	return nil
+func (r *DynamoDiaryRepository) PostDiary(diary *Diary) error {
+	return r.table.Put(diary).Run()
 }
 
 type Scraper interface {

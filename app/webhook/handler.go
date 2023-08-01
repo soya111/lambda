32fd5725b1ea80f9d9cbc2c9a2880d76874de4ee
soya kubodera <49577866+soya111@users.noreply.github.com>
2023-08-01 20:21:03 +0900

    Add help command to display usage of available commands (#26)
    * Add help command to display usage of available commands
    
    * Refactor error handling in Handler methods for consistency

--- app/webhook/handler.go
+++ app/webhook/handler.go
@@ -22,71 +22,150 @@ func NewHandler(client *line.Linebot, db *dynamo.DB, subscriber model.Subscriber
 	return &Handler{client, db, subscriber}
 }
 
+type EventHandler func(ctx context.Context, event *linebot.Event) error
+
+type EventHandlers map[linebot.EventType]EventHandler
+
+func (h *Handler) getEventHandlers() EventHandlers {
+	return EventHandlers{
+		linebot.EventTypeMessage: h.handleMessageEvent,
+		linebot.EventTypeLeave:   h.handleLeaveEvent,
+		// 他のイベントタイプも同様に定義する
+	}
+}
+
 func (h *Handler) HandleEvent(ctx context.Context, event *linebot.Event) error {
-	switch event.Type {
-	case linebot.EventTypeMessage:
-		switch message := event.Message.(type) {
-		case *linebot.TextMessage:
-			err := h.handleTextMessage(message.Text, event)
-			if err != nil {
-				return fmt.Errorf("HandleEvent: %w", err)
-			}
-			return nil
-		}
-	case linebot.EventTypeLeave:
-		// 退会、ブロック等されたら登録情報削除すべき
-		// webhook.HandleEventLeave(event)
+	handler, ok := h.getEventHandlers()[event.Type]
+	if ok {
+		return handler(ctx, event)
+	}
+	// 不明なイベントタイプに対するエラーハンドリング
+	// ...
+	return nil
+}
+
+func (h *Handler) handleMessageEvent(ctx context.Context, event *linebot.Event) error {
+	switch message := event.Message.(type) {
+	case *linebot.TextMessage:
+		return h.handleTextMessage(message.Text, event)
+	default:
+		// TextMessage以外は何もしない
+		return nil
+	}
+}
+
+func (h *Handler) handleLeaveEvent(ctx context.Context, event *linebot.Event) error {
+	// EventTypeLeaveのときの処理を記述
+	// ...
+	return nil
+}
+
+type Command string
+
+const (
+	RegCommand    Command = "reg"
+	UnregCommand  Command = "unreg"
+	ListCommand   Command = "list"
+	WhoamiCommand Command = "whoami"
+	HelpCommand   Command = "help"
+	// 新しいコマンドを追加する場合はここに定義する
+)
+
+type CommandInfo struct {
+	Handler CommandHandler
+	Desc    string
+}
+type CommandHandler func(*linebot.Event, []string) error
+
+type CommandHandlers map[Command]CommandInfo
+
+func (h *Handler) getCommandHandlers() CommandHandlers {
+	return CommandHandlers{
+		RegCommand: {
+			Handler: h.handleRegCommand,
+			Desc:    "Register a member. Usage: reg [member]",
+		},
+		UnregCommand: {
+			Handler: h.handleUnregCommand,
+			Desc:    "Unregister a member. Usage: unreg [member]",
+		},
+		ListCommand: {
+			Handler: h.handleListCommand,
+			Desc:    "Show the list of registered members. Usage: list",
+		},
+		WhoamiCommand: {
+			Handler: h.handleWhoamiCommand,
+			Desc:    "Show your user or group ID. Usage: whoami",
+		},
+		HelpCommand: {
+			Handler: h.handleHelpCommand,
+			Desc:    "Show the list of available commands. Usage: help",
+		},
+		// 新たに追加するコマンドも同様にここに追加します
+	}
+}
+
+func (h *Handler) handleTextMessage(param string, event *linebot.Event) error {
+	params := strings.Split(param, " ")
+	command := Command(params[0])
+	cmdInfo, ok := h.getCommandHandlers()[command]
+	if !ok {
+		return nil
+	}
+	return cmdInfo.Handler(event, params)
+}
+
+func (h *Handler) handleRegCommand(event *linebot.Event, params []string) error {
+	if len(params) < 2 {
+		return nil
+	}
+	if !isMember(params[1]) {
+		return nil
+	}
+	member := params[1]
+	err := h.registerMember(member, event)
+	if err != nil {
+		return fmt.Errorf("handleRegCommand: %w", err)
 	}
 	return nil
 }
 
-func (h *Handler) handleTextMessage(t string, event *linebot.Event) error {
-	text := strings.Split(t, " ")
-	switch {
-	case text[0] == "reg" && isMember(text[1]):
-		member := text[1]
-		err := h.registerMember(member, event)
-		if err != nil {
-			return fmt.Errorf("handleTextMessage: %w", err)
-		}
+func (h *Handler) handleUnregCommand(event *linebot.Event, params []string) error {
+	if len(params) < 2 {
 		return nil
-
-	case text[0] == "unreg" && isMember(text[1]):
-		member := text[1]
-		err := h.unregisterMember(member, event)
-		if err != nil {
-			return fmt.Errorf("handleTextMessage: %w", err)
-		}
+	}
+	if !isMember(params[1]) {
 		return nil
-
-	case text[0] == "reg" && text[1] == "list":
-		err := h.showSubscribeList(event)
-		if err != nil {
-			return fmt.Errorf("handleTextMessage: %w", err)
-		}
-		return nil
-
-	case text[0] == "whoami":
-		switch {
-		case event.Source.Type == linebot.EventSourceTypeUser:
-			h.sendUserId(event)
-		case event.Source.Type == linebot.EventSourceTypeGroup:
-			h.sendGroupId(event)
-		}
-	case isMember(text[0]):
-		// TODO: いつか機能追加
-		// 最新のブログおくるとか
+	}
+	member := params[1]
+	err := h.unregisterMember(member, event)
+	if err != nil {
+		return fmt.Errorf("handleUnregCommand: %w", err)
 	}
 	return nil
 }
 
-func isMember(text string) bool {
-	for _, v := range memberList {
-		if text == v {
-			return true
-		}
+func (h *Handler) handleListCommand(event *linebot.Event, params []string) error {
+	err := h.showSubscribeList(event)
+	if err != nil {
+		return fmt.Errorf("handleListCommand: %w", err)
 	}
-	return false
+	return nil
+}
+
+func (h *Handler) handleWhoamiCommand(event *linebot.Event, params []string) error {
+	return h.sendWhoami(event)
+}
+
+func (h *Handler) handleHelpCommand(event *linebot.Event, params []string) error {
+	replyText := "Available commands are:\n"
+	for command, cmdInfo := range h.getCommandHandlers() {
+		replyText += fmt.Sprintf("%s: %s\n", string(command), cmdInfo.Desc)
+	}
+	if _, err := h.bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyText)).Do(); err != nil {
+		return err
+	}
+	return nil
 }
 
 type Subscriber struct {
@@ -95,106 +174,81 @@ type Subscriber struct {
 }
 
 func (h *Handler) registerMember(member string, event *linebot.Event) error {
-	var id string
+	ctx := context.TODO()
 	token := event.ReplyToken
 
-	if event.Source.Type == linebot.EventSourceTypeUser {
-		// user名調査
-		userId := event.Source.UserID
-		userProfile, _ := h.bot.GetProfile(userId).Do()
-		err := h.postUser(User{userId, userProfile.DisplayName})
-		if err != nil {
-			fmt.Println(err)
-		}
-		id = userId
-	} else if event.Source.Type == linebot.EventSourceTypeGroup {
-		id = event.Source.GroupID
+	id := extractEventSourceIdentifier(event)
+	if id == "" {
+		err := fmt.Errorf("invalid source type: %v", event.Source.Type)
+		return h.bot.ReplyWithError(ctx, token, "Invalid source type!", err)
 	}
 
 	err := h.subscriber.Subscribe(model.Subscriber{member, id})
 	if err != nil {
-		message := "登録できませんでした！"
-		if _, err := h.bot.ReplyMessage(token, linebot.NewTextMessage(message)).Do(); err != nil {
-			return fmt.Errorf("registerMember: %w", err)
-		}
-		return fmt.Errorf("registerMember: %w", err)
+		return h.bot.ReplyWithError(ctx, token, "登録できませんでした！", err)
 	}
 
 	message := fmt.Sprintf("registered %s", member)
-	if _, err := h.bot.ReplyMessage(token, linebot.NewTextMessage(message)).Do(); err != nil {
+	if err := h.bot.ReplyTextMessages(ctx, token, message); err != nil {
 		return fmt.Errorf("registerMember: %w", err)
 	}
 	return nil
 }
 
 func (h *Handler) unregisterMember(member string, event *linebot.Event) error {
+	ctx := context.TODO() // あるいは他の適切なコンテキストを使用します
 	token := event.ReplyToken
+
 	id := extractEventSourceIdentifier(event)
+	if id == "" {
+		err := fmt.Errorf("invalid source type: %v", event.Source.Type)
+		return h.bot.ReplyWithError(ctx, token, "Invalid source type!", err)
+	}
 
 	err := h.subscriber.Unsubscribe(member, id)
 	if err != nil {
-		message := "登録できませんでした！"
-		if _, err := h.bot.ReplyMessage(token, linebot.NewTextMessage(message)).Do(); err != nil {
-			return fmt.Errorf("unregisterMember: %w", err)
-		}
-		return fmt.Errorf("unregisterMember: %w", err)
+		return h.bot.ReplyWithError(ctx, token, "登録解除できませんでした！", err)
 	}
 
 	message := fmt.Sprintf("unregistered %s", member)
-	if _, err := h.bot.ReplyMessage(token, linebot.NewTextMessage(message)).Do(); err != nil {
+	if err := h.bot.ReplyTextMessages(ctx, token, message); err != nil {
 		return fmt.Errorf("unregisterMember: %w", err)
 	}
 	return nil
 }
 
 func (h *Handler) showSubscribeList(event *linebot.Event) error {
+	ctx := context.TODO() // あるいは他の適切なコンテキストを使用します
 	token := event.ReplyToken
+
 	id := extractEventSourceIdentifier(event)
+	if id == "" {
+		err := fmt.Errorf("invalid source type: %v", event.Source.Type)
+		return h.bot.ReplyWithError(ctx, token, "Invalid source type!", err)
+	}
 
 	list, err := h.subscriber.GetAllById(id)
 	if err != nil {
-		message := "情報を取得できませんでした！"
-		if _, err := h.bot.ReplyMessage(token, linebot.NewTextMessage(message)).Do(); err != nil {
-			return fmt.Errorf("showSubscribeList: %w", err)
-		}
-		return fmt.Errorf("showSubscribeList: %w", err)
-
+		return h.bot.ReplyWithError(ctx, token, "情報を取得できませんでした！", err)
 	}
 
 	message := "登録リスト"
 	for _, v := range list {
 		message += fmt.Sprintf("\n%s", v.MemberName)
 	}
-	if _, err := h.bot.ReplyMessage(token, linebot.NewTextMessage(message)).Do(); err != nil {
+	if err := h.bot.ReplyTextMessages(ctx, token, message); err != nil {
 		return fmt.Errorf("showSubscribeList: %w", err)
 	}
 	return nil
 }
 
-func (h *Handler) sendUserId(event *linebot.Event) {
-	message := fmt.Sprintf("User id is \"%s\"", event.Source.UserID)
-	h.bot.ReplyTextMessages(context.TODO(), event.ReplyToken, message)
-}
-
-func (h *Handler) sendGroupId(event *linebot.Event) {
-	message := fmt.Sprintf("Group id is \"%s\"\nYour user id is \"%s\"", event.Source.GroupID, event.Source.UserID)
-	h.bot.ReplyTextMessages(context.TODO(), event.ReplyToken, message)
+func (h *Handler) sendWhoami(event *linebot.Event) error {
+	return h.bot.ReplyTextMessages(context.TODO(), event.ReplyToken, extractEventSourceIdentifier(event))
 }
 
 type User struct {
 	Id   string `json:"user_id" dynamodbav:"user_id"`
 	Name string `json:"name" dynamodbav:"name"`
-}
-
-func (h *Handler) postUser(user User) error {
-	table := h.db.Table("User")
-
-	err := table.Put(user).Run()
-	if err != nil {
-		return fmt.Errorf("postUser: %w", err)
-	}
-
-	return nil
 }
 
 func extractEventSourceIdentifier(event *linebot.Event) string {
---
+++ pkg/infrastructure/line/reply.go
@@ -0,0 +1,22 @@
+package line
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+)
+
+func (b *Linebot) ReplyTextMessages(ctx context.Context, token string, message string) error {
+	if _, err := b.Client.ReplyMessage(token, linebot.NewTextMessage(message)).WithContext(ctx).Do(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *Linebot) ReplyWithError(ctx context.Context, token, replyMessage string, err error) error {
+	if err := b.ReplyTextMessages(ctx, token, replyMessage); err != nil {
+		return fmt.Errorf("replyWithError: %w", err)
+	}
+	return err
+}

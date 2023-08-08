f51dbd4be848ea14741dd85c1d4df127126533b8
soya111 <soyak20000316@gmail.com>
2023-08-09 00:44:26 +0900

    Add error-related tests

--- pkg/infrastructure/scrape/scrape_test.go
+++ pkg/infrastructure/scrape/scrape_test.go
@@ -12,108 +12,123 @@ import (
 
 func TestExtractAndFormatTextFromElement(t *testing.T) {
 	tests := []struct {
-		name     string
-		html     string
-		selector string
-		n        int
-		expected string
+		name      string
+		html      string
+		selector  string
+		n         int
+		expected  string
+		expectErr bool
 	}{
 		{
-			name:     "Simple English text",
-			html:     `<div class="test">Hello, world!</div>`,
-			selector: ".test",
-			n:        12,
-			expected: "Hello, world",
+			name:      "Simple English text",
+			html:      `<div class="test">Hello, world!</div>`,
+			selector:  ".test",
+			n:         12,
+			expected:  "Hello, world",
+			expectErr: false,
 		},
 		{
-			name:     "Two English sentences",
-			html:     `<div class="test">Hello, world!</div><div class="test">Goodbye, world!</div>`,
-			selector: ".test",
-			n:        5,
-			expected: "Hello",
+			name:      "Two English sentences",
+			html:      `<div class="test">Hello, world!</div><div class="test">Goodbye, world!</div>`,
+			selector:  ".test",
+			n:         5,
+			expected:  "Hello",
+			expectErr: false,
 		},
 		{
-			name:     "Simple Japanese text",
-			html:     `<div class="test">こんにちは、世界！</div>`,
-			selector: ".test",
-			n:        5,
-			expected: "こんにちは",
+			name:      "Simple Japanese text",
+			html:      `<div class="test">こんにちは、世界！</div>`,
+			selector:  ".test",
+			n:         5,
+			expected:  "こんにちは",
+			expectErr: false,
 		},
 		{
-			name:     "Mixed Japanese and English text",
-			html:     `<div class="test">日向坂46の加藤史帆です。</div>`,
-			selector: ".test",
-			n:        8,
-			expected: "日向坂46の加藤",
+			name:      "Mixed Japanese and English text",
+			html:      `<div class="test">日向坂46の加藤史帆です。</div>`,
+			selector:  ".test",
+			n:         8,
+			expected:  "日向坂46の加藤",
+			expectErr: false,
 		},
 		{
-			name:     "Nested HTML tags",
-			html:     `<div class="test"><p>日向坂46の加藤史帆です。</p></div>`,
-			selector: ".test",
-			n:        8,
-			expected: "日向坂46の加藤",
+			name:      "Nested HTML tags",
+			html:      `<div class="test"><p>日向坂46の加藤史帆です。</p></div>`,
+			selector:  ".test",
+			n:         8,
+			expected:  "日向坂46の加藤",
+			expectErr: false,
 		},
 		{
-			name:     "Parallel nested HTML tags",
-			html:     `<div class="test"><p>日向坂46の</p><p>加藤史帆です。</p></div>`,
-			selector: ".test",
-			n:        8,
-			expected: "日向坂46の加藤",
+			name:      "Parallel nested HTML tags",
+			html:      `<div class="test"><p>日向坂46の</p><p>加藤史帆です。</p></div>`,
+			selector:  ".test",
+			n:         8,
+			expected:  "日向坂46の加藤",
+			expectErr: false,
 		},
 		{
-			name:     "ID selector",
-			html:     `<div id="test">Hello, world!</div>`,
-			selector: "#test",
-			n:        5,
-			expected: "Hello",
+			name:      "ID selector",
+			html:      `<div id="test">Hello, world!</div>`,
+			selector:  "#test",
+			n:         5,
+			expected:  "Hello",
+			expectErr: false,
 		},
 		{
-			name:     "Invalid selector",
-			html:     `<div class="test">Hello, world!</div>`,
-			selector: ".invalid",
-			n:        12,
-			expected: "",
+			name:      "Invalid selector",
+			html:      `<div class="test">Hello, world!</div>`,
+			selector:  ".invalid",
+			n:         12,
+			expected:  "",
+			expectErr: true,
 		},
 		{
-			name:     "Empty text",
-			html:     `<div class="test"></div>`,
-			selector: ".test",
-			n:        5,
-			expected: "",
+			name:      "Empty text",
+			html:      `<div class="test"></div>`,
+			selector:  ".test",
+			n:         5,
+			expected:  "",
+			expectErr: true,
 		},
 		{
-			name:     "Whitespace only text",
-			html:     `<div class="test">     </div>`,
-			selector: ".test",
-			n:        5,
-			expected: "",
+			name:      "Whitespace only text",
+			html:      `<div class="test">     </div>`,
+			selector:  ".test",
+			n:         5,
+			expected:  " ",
+			expectErr: false,
 		},
 		{
-			name:     "Text with leading and trailing whitespaces",
-			html:     `  <div class="test">   Hello   </div>  `,
-			selector: ".test",
-			n:        8,
-			expected: "Hello",
+			name:      "Text with leading and trailing whitespaces",
+			html:      `  <div class="test">   Hello   </div>  `,
+			selector:  ".test",
+			n:         8,
+			expected:  "Hello",
+			expectErr: false,
 		},
 		{
-			name:     "MaxLength set to 0",
-			html:     `<div class="test">Hello, world!</div>`,
-			selector: ".test",
-			n:        0,
-			expected: "",
+			name:      "MaxLength set to 0",
+			html:      `<div class="test">Hello, world!</div>`,
+			selector:  ".test",
+			n:         0,
+			expected:  "",
+			expectErr: false,
 		},
 		{
-			name:     "Text length same as MaxLength",
-			html:     `<div class="test">Hello</div>`,
-			selector: ".test",
-			n:        5,
-			expected: "Hello",
+			name:      "Text length same as MaxLength",
+			html:      `<div class="test">Hello</div>`,
+			selector:  ".test",
+			n:         5,
+			expected:  "Hello",
+			expectErr: false,
 		},
 	}
 
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			tt := tt
+			t.Parallel()
 			doc, err := goquery.NewDocumentFromReader(strings.NewReader(tt.html))
 			if err != nil {
 				t.Fatal(err)
@@ -123,9 +138,11 @@ func TestExtractAndFormatTextFromElement(t *testing.T) {
 			}
 
 			result, err := scrape.ExtractAndFormatTextFromElement(doc, tt.selector, opt)
-			assert.NoError(t, err)
-			if result != tt.expected {
-				t.Errorf("Expected '%s', got '%s'", tt.expected, result)
+			if tt.expectErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.expected, result, "Expected '%s', got '%s'", tt.expected, result)
 			}
 		})
 	}

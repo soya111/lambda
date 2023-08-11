06e0db25c7a5675bf34e9bed2ca5f0909170c770
soya111 <soyak20000316@gmail.com>
2023-08-12 02:02:16 +0900

    Add tests using testify for ExtractEventSourceIdentifier function

---
+++ pkg/infrastructure/line/event_test.go
@@ -0,0 +1,48 @@
+package line_test
+
+import (
+	"notify/pkg/infrastructure/line"
+	"testing"
+
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExtractEventSourceIdentifier(t *testing.T) {
+	tests := []struct {
+		name   string
+		source linebot.EventSource
+		want   string
+	}{
+		{
+			name:   "User Source",
+			source: linebot.EventSource{Type: linebot.EventSourceTypeUser, UserID: "userID123"},
+			want:   "userID123",
+		},
+		{
+			name:   "Group Source",
+			source: linebot.EventSource{Type: linebot.EventSourceTypeGroup, GroupID: "groupID123"},
+			want:   "groupID123",
+		},
+		{
+			name:   "Room Source",
+			source: linebot.EventSource{Type: linebot.EventSourceTypeRoom, RoomID: "roomID123"},
+			want:   "roomID123",
+		},
+		{
+			name:   "Unknown Source",
+			source: linebot.EventSource{Type: "unknown"},
+			want:   "",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			event := &linebot.Event{Source: &tt.source}
+			got := line.ExtractEventSourceIdentifier(event)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}

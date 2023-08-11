08a2bb3e842ac7e6060e157a54d59ecdab53f445
soya111 <soyak20000316@gmail.com>
2023-08-11 21:08:25 +0900

    Add success logging to PushMessages function

---
+++ pkg/infrastructure/line/push.go
@@ -0,0 +1,25 @@
+package line
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+)
+
+// PushMessages push messages to line
+func (b *Linebot) PushMessages(ctx context.Context, to []string, messages ...linebot.SendingMessage) error {
+	var result *multierror.Error
+
+	for _, to := range to {
+		response, err := b.Client.PushMessage(to, messages...).WithContext(ctx).Do()
+		if err != nil {
+			result = multierror.Append(result, err)
+			fmt.Printf("Push Messages to %s, error: %v\n", to, err)
+		} else {
+			fmt.Printf("Push Messages to %s, success, response: %v\n", to, response)
+		}
+	}
+	return result.ErrorOrNil()
+}

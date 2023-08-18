f17dde670a444d505625449e04c012773697a883
soya111 <soyak20000316@gmail.com>
2023-08-19 04:57:52 +0900

    Create logging package to integrate custom logger with context

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/line/line-bot-sdk-go/v7 v7.20.0
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d
 	github.com/stretchr/testify v1.7.2
+	go.uber.org/zap v1.17.0
 	golang.org/x/net v0.14.0
 )
 
@@ -23,6 +24,8 @@ require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ pkg/logging/logger.go
@@ -0,0 +1,59 @@
+package logging
+
+import (
+	"context"
+	"fmt"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var loggerKey = struct{}{}
+
+// InitializeLogger returns a logger instance.
+func InitializeLogger() *zap.Logger {
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "", // 時刻情報を表示しない
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		EncodeLevel:    zapcore.CapitalLevelEncoder,
+		EncodeTime:     nil, // 時刻情報のエンコードを省略
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder, // ファイル名と行数を表示
+	}
+
+	atom := zap.NewAtomicLevelAt(zap.DebugLevel)
+
+	config := zap.Config{
+		Level:            atom,
+		Development:      true,
+		Encoding:         "json",
+		EncoderConfig:    encoderConfig,
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+
+	l, err := config.Build(zap.AddCaller()) // AddCallerオプションでcaller情報を追加
+	if err != nil {
+		panic(err)
+	}
+	return l
+}
+
+// ContextWithLogger returns a new context with the logger.
+func ContextWithLogger(ctx context.Context, logger *zap.Logger) context.Context {
+	return context.WithValue(ctx, loggerKey, logger)
+}
+
+// LoggerFromContext returns a logger from the context.
+func LoggerFromContext(ctx context.Context) *zap.Logger {
+	logger, ok := ctx.Value(loggerKey).(*zap.Logger)
+	if !ok {
+		fmt.Println("logger not found in context")
+		return zap.L() // デフォルトロガーを返す
+	}
+	return logger
+}

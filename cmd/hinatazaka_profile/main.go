1a27c13f6b4728ca3581269642fc0ddbbc092f3f
Toya321 <toya23234949@gmail.com>
2023-08-21 02:38:36 +0900

    change variable name and add some comments

--- cmd/hinatazaka_profile/main.go
+++ cmd/hinatazaka_profile/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"strings"
 
 	"notify/pkg/infrastructure/scrape"
@@ -13,8 +12,8 @@ import (
 
 // プロフィールのstruct
 type profile struct {
-	X string
-	Y string
+	entry string // 生年月日、星座などのプロフィール項目
+	value string //具体的な値
 }
 
 var member [6]profile
@@ -25,7 +24,7 @@ func main() {
 
 	if !model.IsMember(name) {
 		fmt.Println("人名でない文字列もしくは日向坂46に存在しないメンバーです。")
-		os.Exit(0)
+		return
 	}
 
 	url := "https://www.hinatazaka46.com/s/official/artist/" + model.MemberToIdMap[name] + "?ima=0000" // 任意のメンバーのURL
@@ -35,26 +34,27 @@ func main() {
 	doc := document.Find(".l-contents")
 
 	// セレクタを使って要素を抽出
-	cnty := 0
+	cnt_v := 0
 	doc.Find(".c-member__info-td__text").Each(func(index int, element *goquery.Selection) {
-		y := strings.TrimSpace(element.Text())
-		member[cnty].Y = y
-		cnty += 1
+		v := strings.TrimSpace(element.Text())
+		member[cnt_v].value = v
+		cnt_v += 1
 	})
 
-	cntx := 0
+	cnt_e := 0
 	doc.Find(".c-member__info-td__name").Each(func(index int, element *goquery.Selection) {
-		x := strings.TrimSpace(element.Text())
-		member[cntx].X = x
-		cntx += 1
+		e := strings.TrimSpace(element.Text())
+		member[cnt_e].entry = e
+		cnt_e += 1
 	})
 
 	fmt.Println(name)
 
-	for index, m := range member {
-		fmt.Printf("%s:%s", m.X, m.Y)
+	//プロフィールの項目と値をカンマで区切り出力
+	for index, prof := range member {
+		fmt.Printf("%s:%s", prof.entry, prof.value)
 		if index == 4 {
-			os.Exit(0)
+			return //不要な項目を含むため途中で終了
 		}
 		fmt.Printf(", ")
 	}

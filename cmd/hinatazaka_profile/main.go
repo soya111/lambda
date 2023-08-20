91825bd682ae51a8bef139cc95310906a241427e
Toya321 <142418961+Toya321@users.noreply.github.com>
2023-08-21 03:35:55 +0900

    add hinatazaka_profile (#47)
    * add hinatazaka_profile
    
    * reviced
    
    * add README about hinatazaka_profile
    
    * Responded to unexpected input
    
    * Fixed a bug
    
    * changed output format
    
    * change variable name and add some comments
    
    * remove underscores

---
+++ cmd/hinatazaka_profile/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"notify/pkg/infrastructure/scrape"
+	"notify/pkg/model"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+// プロフィールのstruct
+type profile struct {
+	entry string // 生年月日、星座などのプロフィール項目
+	value string //具体的な値
+}
+
+var member [6]profile
+
+func main() {
+	var name string
+	fmt.Scan(&name) //任意のメンバーを入力
+
+	if !model.IsMember(name) {
+		fmt.Println("人名でない文字列もしくは日向坂46に存在しないメンバーです。")
+		return
+	}
+
+	url := "https://www.hinatazaka46.com/s/official/artist/" + model.MemberToIdMap[name] + "?ima=0000" // 任意のメンバーのURL
+
+	document, _ := scrape.GetDocumentFromURL(url)
+
+	doc := document.Find(".l-contents")
+
+	// セレクタを使って要素を抽出
+	cntv := 0
+	doc.Find(".c-member__info-td__text").Each(func(index int, element *goquery.Selection) {
+		v := strings.TrimSpace(element.Text())
+		member[cntv].value = v
+		cntv += 1
+	})
+
+	cnte := 0
+	doc.Find(".c-member__info-td__name").Each(func(index int, element *goquery.Selection) {
+		e := strings.TrimSpace(element.Text())
+		member[cnte].entry = e
+		cnte += 1
+	})
+
+	fmt.Println(name)
+
+	//プロフィールの項目と値をカンマで区切り出力
+	for index, prof := range member {
+		fmt.Printf("%s:%s", prof.entry, prof.value)
+		if index == 4 {
+			return //不要な項目を含むため途中で終了
+		}
+		fmt.Printf(", ")
+	}
+}

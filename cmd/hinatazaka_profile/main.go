ee4d9dbfde59fdcd82d9dc31a3cb48ecff7feec9
Toya321 <142418961+Toya321@users.noreply.github.com>
2023-08-25 20:04:54 +0900

    bugfix and support command argument (#49)
    * bugfix and support command argument
    
    * change pointer type
    
    * functionalize each element
    
    * add error types
    
    * change error type
    
    * add testcode
    
    * fix error type
    
    * change  error type
    
    * change capital letter

--- cmd/hinatazaka_profile/main.go
+++ cmd/hinatazaka_profile/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"strings"
 
@@ -16,46 +18,99 @@ type profile struct {
 	value string //具体的な値
 }
 
-var member [6]profile
+var (
+	ErrNonExistentMember = errors.New("日向坂46に存在しないメンバーです。")
+	ErrNoUrl             = errors.New("ポカは日向坂46の一員ですが、URLが存在しません。")
+)
 
-func main() {
-	var name string
-	fmt.Scan(&name) //任意のメンバーを入力
+// ポカのプロフィール
+var PokaProfile = [6]profile{
+	{"生年月日", "2019年12月25日"},
+	{"星座", "やぎ座"},
+	{"身長", "???"},
+	{"出身地", "???"},
+	{"血液型", "???"},
+}
+var name string
 
+func init() {
+	flag.StringVar(&name, "name", "hinata", "名前を入力してください")
+}
+
+// inputNameはコマンド引数により名前を取得
+func inputName() string {
+	flag.Parse()
+	return name
+}
+
+// getProfileSelectionはメンバーごとのプロフィールが記載されたセレクションを取得
+func getProfileSelection(name string) (*goquery.Selection, error) {
+	//入力がメンバー名でない場合
 	if !model.IsMember(name) {
-		fmt.Println("人名でない文字列もしくは日向坂46に存在しないメンバーです。")
-		return
+		return nil, ErrNonExistentMember
+	}
+
+	//入力がポカである場合
+	if model.MemberToIdMap[name] == "000" {
+		return nil, ErrNoUrl
 	}
 
 	url := "https://www.hinatazaka46.com/s/official/artist/" + model.MemberToIdMap[name] + "?ima=0000" // 任意のメンバーのURL
-
 	document, _ := scrape.GetDocumentFromURL(url)
+	selection := document.Find(".l-contents")
 
-	doc := document.Find(".l-contents")
+	return selection, nil
+}
 
-	// セレクタを使って要素を抽出
+// scrapeProfileはセレクションからスクレイピングしたプロフィールを取得
+func scrapeProfile(selection *goquery.Selection) [6]profile {
+	var member [6]profile
 	cntv := 0
-	doc.Find(".c-member__info-td__text").Each(func(index int, element *goquery.Selection) {
+	cnte := 0
+
+	//セレクタを使って要素を抽出
+	selection.Find(".c-member__info-td__text").Each(func(index int, element *goquery.Selection) {
 		v := strings.TrimSpace(element.Text())
 		member[cntv].value = v
 		cntv += 1
 	})
 
-	cnte := 0
-	doc.Find(".c-member__info-td__name").Each(func(index int, element *goquery.Selection) {
+	selection.Find(".c-member__info-td__name").Each(func(index int, element *goquery.Selection) {
 		e := strings.TrimSpace(element.Text())
 		member[cnte].entry = e
 		cnte += 1
 	})
 
-	fmt.Println(name)
+	return member
+}
+
+// outputProfileはプロフィールを標準形で出力
+func outputProfile(name string, member [6]profile) {
+	fmt.Println(name) //メンバーの名前を出力
 
 	//プロフィールの項目と値をカンマで区切り出力
-	for index, prof := range member {
-		fmt.Printf("%s:%s", prof.entry, prof.value)
-		if index == 4 {
-			return //不要な項目を含むため途中で終了
-		}
-		fmt.Printf(", ")
+	var profile []string
+	for _, prof := range member[:5] {
+		profile = append(profile, prof.entry+":"+prof.value)
 	}
+
+	fmt.Println(strings.Join(profile, ", "))
+}
+
+func main() {
+	name := inputName()
+	selection, err := getProfileSelection(name)
+
+	if err != nil {
+		if errors.Is(err, ErrNoUrl) {
+			outputProfile(name, PokaProfile)
+		} else {
+			fmt.Println(err)
+		}
+		return
+	}
+
+	member := scrapeProfile(selection)
+
+	outputProfile(name, member)
 }
---
+++ cmd/hinatazaka_profile/main_test.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_inputName(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{"Nomal", "潮紗理菜", "潮紗理菜"},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			err := flag.CommandLine.Set("name", tt.input)
+			if err != nil {
+				fmt.Printf("Error setting command-line arguments: %v\n", err)
+			}
+			actual := inputName()
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
+
+func Test_getProfileSelection(t *testing.T) {
+	tests := []struct {
+		name          string
+		input         string
+		expectederror error
+	}{
+		{"ExistentMember", "潮紗理菜", nil},
+		{"NonExistentMember", "白石麻衣", ErrNonExistentMember},
+		{"ポカ", "ポカ", ErrNoUrl},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			_, actual := getProfileSelection(tt.input)
+			assert.Equal(t, tt.expectederror, actual)
+		})
+	}
+}
+
+func Test_scrapeProfile(t *testing.T) {
+	type profile struct {
+		entry string
+		value string
+	}
+
+	var UsiosarinaProfile = [6]profile{
+		{"生年月日", "1997年12月26日"},
+		{"星座", "やぎ座"},
+		{"身長", "157.5cm"},
+		{"出身地", "神奈川県"},
+		{"血液型", "O型"},
+		{"SNS", ""},
+	}
+
+	tests := []struct {
+		name     string
+		input    string
+		expected [6]profile
+	}{
+		{"Nornal", "潮紗理菜", UsiosarinaProfile},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			selection, _ := getProfileSelection(tt.input)
+			actual := scrapeProfile(selection)
+			for i := 0; i < 6; i++ {
+				assert.Equal(t, tt.expected[i].entry, actual[i].entry)
+				assert.Equal(t, tt.expected[i].value, actual[i].value)
+			}
+		})
+	}
+}
+
+func Test_outputProfile(t *testing.T) {
+	var UsiosarinaProfile = [6]profile{
+		{"生年月日", "1997年12月26日"},
+		{"星座", "やぎ座"},
+		{"身長", "157.5cm"},
+		{"出身地", "神奈川県"},
+		{"血液型", "O型"},
+		{"SNS", ""},
+	}
+
+	var PokaProfile = [6]profile{
+		{"生年月日", "2019年12月25日"},
+		{"星座", "やぎ座"},
+		{"身長", "???"},
+		{"出身地", "???"},
+		{"血液型", "???"},
+	}
+
+	tests := []struct {
+		name          string
+		inputname     string
+		inputparofile [6]profile
+		expected      string
+	}{
+		{"Nornal", "潮紗理菜", UsiosarinaProfile, "潮紗理菜\n生年月日:1997年12月26日, 星座:やぎ座, 身長:157.5cm, 出身地:神奈川県, 血液型:O型\n"},
+		{"ポカ", "ポカ", PokaProfile, "ポカ\n生年月日:2019年12月25日, 星座:やぎ座, 身長:???, 出身地:???, 血液型:???\n"},
+	}
+
+	for _, tt := range tests {
+		// 標準出力をキャプチャ
+		old := os.Stdout
+		r, w, _ := os.Pipe()
+		os.Stdout = w
+
+		// テスト対象の関数呼び出し
+		outputProfile(tt.inputname, tt.inputparofile)
+
+		// 標準出力を元に戻す
+		w.Close()
+		os.Stdout = old
+
+		// キャプチャした出力を読み取る
+		var capturedOutput string
+		buf := new(bytes.Buffer)
+		_, _ = buf.ReadFrom(r)
+		capturedOutput = buf.String()
+
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			actual := capturedOutput
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}

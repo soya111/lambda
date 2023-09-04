0b6d0d478558d1fcdc607f346878ba1b42eb8324
soya kubodera <49577866+soya111@users.noreply.github.com>
2023-09-04 22:16:40 +0900

    Feature/optimize logging (#61)
    * Optimize Logging in Push Messages
    
    * Optimize Logging in reply messages

--- pkg/infrastructure/line/push.go
+++ pkg/infrastructure/line/push.go
@@ -2,24 +2,31 @@ package line
 
 import (
 	"context"
-	"fmt"
+	"notify/pkg/logging"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"go.uber.org/zap"
 )
 
 // PushMessages push messages to LINE
 func (b *Linebot) PushMessages(ctx context.Context, to []string, messages ...linebot.SendingMessage) error {
 	var result *multierror.Error
-
-	for _, to := range to {
-		response, err := b.Client.PushMessage(to, messages...).WithContext(ctx).Do()
+	logger := logging.LoggerFromContext(ctx)
+	logger.Info("Start pushing messages to LINE.", zap.Any("messages", messages))
+	for _, recipient := range to {
+		_, err := b.Client.PushMessage(recipient, messages...).WithContext(ctx).Do()
 		if err != nil {
 			result = multierror.Append(result, err)
-			fmt.Printf("Push Messages to %s, error: %v\n", to, err)
+			logger.Error("Failed to push messages to LINE.", zap.Error(err), zap.String("recipient", recipient))
 		} else {
-			fmt.Printf("Push Messages to %s, success, response: %v\n", to, response)
+			logger.Info("Successfully pushed messages to LINE.", zap.String("recipient", recipient))
 		}
+	}
+	if result != nil {
+		logger.Warn("Failed to push messages to LINE for some users.")
+	} else {
+		logger.Info("All messages successfully pushed.")
 	}
 	return result.ErrorOrNil()
 }
--- pkg/infrastructure/line/reply.go
+++ pkg/infrastructure/line/reply.go
@@ -3,30 +3,41 @@ package line
 import (
 	"context"
 	"fmt"
+	"notify/pkg/logging"
 
 	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"go.uber.org/zap"
 )
 
 // ReplyTextMessages reply text messages to LINE
 func (b *Linebot) ReplyTextMessages(ctx context.Context, token string, message string) error {
+	logger := logging.LoggerFromContext(ctx)
 	if _, err := b.Client.ReplyMessage(token, linebot.NewTextMessage(message)).WithContext(ctx).Do(); err != nil {
+		logger.Error("Failed to reply text message to LINE.", zap.Error(err), zap.String("token", token), zap.String("message", message))
 		return err
 	}
+	logger.Info("Successfully replied text message to LINE.", zap.String("token", token), zap.String("message", message))
 	return nil
 }
 
 // ReplyMessage reply messages to LINE
 func (b *Linebot) ReplyMessage(ctx context.Context, token string, messages ...linebot.SendingMessage) error {
+	logger := logging.LoggerFromContext(ctx)
 	if _, err := b.Client.ReplyMessage(token, messages...).WithContext(ctx).Do(); err != nil {
+		logger.Error("Failed to reply messages to LINE.", zap.Error(err), zap.String("token", token), zap.Any("messages", messages))
 		return err
 	}
+	logger.Info("Successfully replied messages to LINE.", zap.String("token", token))
 	return nil
 }
 
 // ReplyWithError reply message to LINE and return error
 func (b *Linebot) ReplyWithError(ctx context.Context, token, replyMessage string, err error) error {
+	logger := logging.LoggerFromContext(ctx)
 	if err := b.ReplyTextMessages(ctx, token, replyMessage); err != nil {
+		logger.Error("Failed to reply with error message to LINE.", zap.Error(err), zap.String("token", token), zap.String("replyMessage", replyMessage))
 		return fmt.Errorf("replyWithError: %w", err)
 	}
+	logger.Info("Successfully replied with error message to LINE.", zap.String("token", token), zap.String("replyMessage", replyMessage))
 	return err
 }

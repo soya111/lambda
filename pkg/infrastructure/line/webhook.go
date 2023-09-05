90508a2fb4c69fae081641d0e98d396a74e52e14
soya111 <soyak20000316@gmail.com>
2023-09-05 21:52:51 +0900

    Integrate local and Lambda execution

--- pkg/infrastructure/line/webhook.go
+++ pkg/infrastructure/line/webhook.go
@@ -9,13 +9,21 @@ import (
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
+// RequestParser is an interface for parsing request
+type RequestParser interface {
+	ParseRequest(req *http.Request) ([]*linebot.Event, error)
+}
+
+// LocalParser is a parser for local development
+type LocalParser struct{}
+
 // ParseRequest without signature validation
 // This function is used for testing
 // Dangerous: This function does not validate the signature
 // If you want to validate the signature, use ParseRequest instead
 // When you get dangerous message, you should exit the program
 // TODO: remove this function
-func ParseRequestWithoutSignatureValidation(r *http.Request) ([]*linebot.Event, error) {
+func (p *LocalParser) ParseRequest(r *http.Request) ([]*linebot.Event, error) {
 	fmt.Println("Dangerous: This function does not validate the signature")
 
 	defer r.Body.Close()

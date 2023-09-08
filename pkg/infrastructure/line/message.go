f97173fccc4ba79645360dbfcf186bc98045d0d1
Toya321 <toya23234949@gmail.com>
2023-09-08 23:44:02 +0900

    improve membericon quality

--- pkg/infrastructure/line/message.go
+++ pkg/infrastructure/line/message.go
@@ -52,22 +52,23 @@ func createFlexTextMessage(diary *blog.ScrapedDiary, showNewLabel bool) *linebot
 	container.Body = &linebot.BoxComponent{
 		Type:       linebot.FlexComponentTypeBox,
 		Layout:     linebot.FlexBoxLayoutTypeVertical,
+		Height:     "450px",
 		PaddingAll: "0px",
 		Contents: []linebot.FlexComponent{
 			&linebot.BoxComponent{
 				Type:   linebot.FlexComponentTypeBox,
 				Layout: linebot.FlexBoxLayoutTypeVertical,
+				Height: "70%",
 				Contents: []linebot.FlexComponent{
 					&linebot.BoxComponent{
 						Type:   linebot.FlexComponentTypeBox,
 						Layout: linebot.FlexBoxLayoutTypeHorizontal,
 						Contents: []linebot.FlexComponent{
 							&linebot.ImageComponent{
-								Type:        linebot.FlexComponentTypeImage,
-								URL:         diary.MemberIcon,
-								Size:        linebot.FlexImageSizeTypeFull,
-								AspectMode:  linebot.FlexImageAspectModeTypeCover,
-								AspectRatio: linebot.FlexImageAspectRatioType4to3,
+								Type:       linebot.FlexComponentTypeImage,
+								URL:        diary.MemberIcon,
+								Size:       linebot.FlexImageSizeTypeFull,
+								AspectMode: linebot.FlexImageAspectModeTypeCover,
 							},
 						},
 					},
